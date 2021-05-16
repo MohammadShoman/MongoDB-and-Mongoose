dfdbbbd2ee1009d8751e9562b970b37d32fa7fcd
MohammadShoman <mohammad.shoman25@gmail.com>
2021-05-16 13:30:29 -0700

    pulse check done

---
+++ schema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const todoSchema=new mongoose.Schema({
+    task:{type:String,required:true},
+    description:{type:String},
+    deadline:{type:String,required:true},
+    isCompleted:{type:Boolean,required:true},
+    priority:{type:String,required:true}
+
+})
+
+module.exports=mongoose.model("todo",todoSchema)
